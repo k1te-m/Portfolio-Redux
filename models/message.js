2b7b5989ea8ac1321b5739071a0d62ad7368fa80
k1te <kmiller343@gmail.com>
2021-02-12 17:39:15 -0600

    created Message and Repo mongoose models

---
+++ models/message.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  message: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
