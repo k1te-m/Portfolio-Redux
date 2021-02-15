385499e8823b5b55aa698f455c3cacc59681e564
k1te <kmiller343@gmail.com>
2021-02-15 15:26:41 -0600

    API util for sending message

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,4 +4,7 @@ export default {
   getRepos: async () => {
     return await axios.get("/api/repos");
   },
+  sendMessage: async (data) => {
+    return await axios.post("/api/messages", data);
+  },
 };
