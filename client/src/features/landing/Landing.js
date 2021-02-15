e04b72740b52fdb06dbe0b6f5b376b600e283abf
k1te <kmiller343@gmail.com>
2021-02-15 14:37:14 -0600

    render loading component if repos are loading

--- client/src/features/landing/Landing.js
+++ client/src/features/landing/Landing.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Jumbotron from "../landing/jumbotron/Jumbotron";
+import Portfolio from "./portfolio/Portfolio";
+import Loading from "../loading/Loading";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectPortfolioLoading,
+  loadRepos,
+} from "../landing/portfolio/portfolioSlice";
 
 const Landing = () => {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <Jumbotron />
+  const isPortfolioLoading = useSelector(selectPortfolioLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRepos());
+  }, []);
+
+  if (isPortfolioLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <Jumbotron />
+        </div>
+        <div className="container">
+          <Portfolio />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Landing;
