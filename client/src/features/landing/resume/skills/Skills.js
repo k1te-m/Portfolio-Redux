f3a35e9e5e6f63a74aec668a9a75f4a3ede7d964
k1te <kmiller343@gmail.com>
2021-02-15 17:08:57 -0600

    updated CSS for resume section

--- client/src/features/landing/resume/skills/Skills.js
+++ client/src/features/landing/resume/skills/Skills.js
@@ -3,23 +3,30 @@ import styled from "styled-components";
 
 const Skills = () => {
   return (
-    <>
-      <h1>Skills</h1>
-
-      <div className="card">
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">HTML / CSS / Bootstrap</li>
-          <li className="list-group-item">JavaScript / jQuery / Node.js</li>
-          <li className="list-group-item">MySQL / MongoDB / Express / Jest</li>
-          <li className="list-group-item">Git / React.js / Redux.js</li>
-          <li className="list-group-item">React-Redux.js / ReduxToolkit.js</li>
-          <li className="list-group-item">Proficient with MERN stack</li>
-          <li className="list-group-item">
-            Excellent written & verbal communication skills
-          </li>
-        </ul>
+    <div className="container">
+      <div className="row">
+        <h1>Skills</h1>
       </div>
-    </>
+      <div className="row">
+        <div className="card">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">HTML / CSS / Bootstrap</li>
+            <li className="list-group-item">JavaScript / jQuery / Node.js</li>
+            <li className="list-group-item">
+              MySQL / MongoDB / Express / Jest
+            </li>
+            <li className="list-group-item">Git / React.js / Redux.js</li>
+            <li className="list-group-item">
+              React-Redux.js / ReduxToolkit.js
+            </li>
+            <li className="list-group-item">Proficient with MERN stack</li>
+            <li className="list-group-item">
+              Excellent written & verbal communication skills
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 };
 
