8a6c115e805e76494ba02694823e7eb818e4f88e
k1te <kmiller343@gmail.com>
2021-02-15 15:11:47 -0600

    imported modal reducer to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import portfolioReducer from "../features/landing/portfolio/portfolioSlice";
+import modalReducer from "../features/modal/modalSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     portfolio: portfolioReducer,
+    modal: modalReducer,
   },
 });
