acaf85f92094c1304b5507a4caf0d9db474a931a
k1te <kmiller343@gmail.com>
2021-02-15 14:36:42 -0600

    imported portfolioReducer to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import portfolioReducer from "../features/landing/portfolio/portfolioSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    portfolio: portfolioReducer,
   },
 });
