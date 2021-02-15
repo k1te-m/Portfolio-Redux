c1993c5c013d6fed84a6dd2a2b87ad0584f9026d
k1te <kmiller343@gmail.com>
2021-02-15 16:20:48 -0600

    basic skills component

---
+++ client/src/features/landing/resume/skills/Skills.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const Skills = () => {
+  return (
+    <>
+      <h1>Skills</h1>
+
+      <div className="card">
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">HTML / CSS / Bootstrap</li>
+          <li className="list-group-item">JavaScript / jQuery / Node.js</li>
+          <li className="list-group-item">MySQL / MongoDB / Express / Jest</li>
+          <li className="list-group-item">Git / React.js / Redux.js</li>
+          <li className="list-group-item">React-Redux.js / ReduxToolkit.js</li>
+          <li className="list-group-item">Proficient with MERN stack</li>
+          <li className="list-group-item">
+            Excellent written & verbal communication skills
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
