c640a13099920d9f16fa714fef91b105be2957fb
k1te <kmiller343@gmail.com>
2021-02-16 12:37:07 -0600

    removed all useRef hooks, imported Footer

--- client/src/features/landing/Landing.js
+++ client/src/features/landing/Landing.js
@@ -12,6 +12,7 @@ import Contact from "./contact/Contact";
 import Education from "./resume/education/Education";
 import Skills from "./resume/skills/Skills";
 import WorkHistory from "./resume/workhistory/WorkHistory";
+import Footer from "./footer/Footer";
 
 const FooterStyled = styled.footer``;
 
@@ -22,16 +23,6 @@ const PortfolioSection = styled.div`
 const Landing = () => {
   const isPortfolioLoading = useSelector(selectPortfolioLoading);
   const dispatch = useDispatch();
-
-  const portfolioBegin = useRef(null);
-  const contactBegin = useRef(null);
-  const educationBegin = useRef(null);
-  const skillsBegin = useRef(null);
-  const experienceBegin = useRef(null);
-
-  const scrollToSection = (section) => {
-    section.current?.scrollIntoView({ behavior: "smooth" });
-  };
 
   useEffect(() => {
     dispatch(loadRepos());
@@ -48,90 +39,27 @@ const Landing = () => {
           </div>
         </div>
         <div className="container">
-          <PortfolioSection className="row" ref={portfolioBegin}>
+          <PortfolioSection className="row" id="portfolio">
             <Portfolio />
           </PortfolioSection>
           <hr />
-          <div className="row" ref={contactBegin}>
+          <div className="row" id="contact">
             <Contact />
           </div>
           <hr />
-          <div className="row" ref={educationBegin}>
+          <div className="row" id="education">
             <Education />
           </div>
           <hr />
-          <div className="row" ref={skillsBegin}>
+          <div className="row" id="skills">
             <Skills />
           </div>
           <hr />
-          <div className="row" ref={experienceBegin}>
+          <div className="row" id="workHistory">
             <WorkHistory />
           </div>
           <div className="row">
-            <FooterStyled className="footer font-small">
-              <div className="container text-center">
-                <hr className="w-100" />
-                <div className="row">
-                  <i className="fab fa-github" />
-                </div>
-                <div className="row">
-                  <div className="col">
-                    <div className="row">
-                      <a onClick={() => scrollToSection(portfolioBegin)}>
-                        Portfolio
-                      </a>
-                    </div>
-                    <div className="row">
-                      <a onClick={() => scrollToSection(contactBegin)}>
-                        Contact
-                      </a>
-                    </div>
-                    <div className="row">
-                      <a onClick={() => scrollToSection(educationBegin)}>
-                        Education
-                      </a>
-                    </div>
-                    <div className="row">
-                      <a onClick={() => scrollToSection(skillsBegin)}>Skills</a>
-                    </div>
-                  </div>
-                  <div className="col">
-                    <div className="row">
-                      <a onClick={() => scrollToSection(experienceBegin)}>
-                        Experience
-                      </a>
-                    </div>
-                    <div className="row">
-                      <a
-                        href="https://www.linkedin.com/in/kmiller29/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        LinkedIn
-                      </a>
-                    </div>
-                    <div className="row">
-                      <a
-                        href="https://github.com/k1te-m"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        GitHub
-                      </a>
-                    </div>
-                    <div className="row">
-                      <a
-                        href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Resume
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </FooterStyled>
+            <Footer />
           </div>
         </div>
       </>
