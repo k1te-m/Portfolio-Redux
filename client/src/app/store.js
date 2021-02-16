6b32819275ca4a76689b253ac964356ba1ae22f9
k1te <kmiller343@gmail.com>
2021-02-16 13:25:39 -0600

    import navReducer to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import portfolioReducer from "../features/landing/portfolio/portfolioSlice";
 import modalReducer from "../features/modal/modalSlice";
+import navReducer from "../features/landing/nav/navSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     portfolio: portfolioReducer,
     modal: modalReducer,
+    nav: navReducer,
   },
 });
