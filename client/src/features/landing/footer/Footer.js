214a59310a72dc09d5ed0af335a423d792157471
k1te <kmiller343@gmail.com>
2021-02-18 13:16:03 -0600

    recreated footer component

--- client/src/features/landing/footer/Footer.js
+++ client/src/features/landing/footer/Footer.js
@@ -1,81 +1,55 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { HashLink } from "react-router-hash-link";
+
+const pulse = keyframes`
+  from { transfrom: scale(1); }
+  50% { transform: scale(.85); }
+  to { transform: scale(1);}
+`;
 
 const FooterWrapper = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
   text-align: center;
-`;
+  justify-content: center;
+  background-color: #65ccb8;
+  align-items: center;
+  height: 128px;
 
-const FooterStyled = styled.footer`
-  width: 100vw;
+  i {
+    font-size: 4rem;
+    color: white;
+  }
+  i:hover {
+    animation: ${pulse} 1s infinite;
+  }
+  a {
+    width: 150px;
+  }
 `;
 
 const Footer = () => {
   return (
-    <FooterWrapper>
-      <FooterStyled className="footer font-small">
-        <div className="container-fluid">
-          <hr className="w-100" />
-          <div className="row">
-            <i className="fab fa-github" />
-          </div>
-          <div className="row">
-            <div className="col-3">
-              <div className="row">
-                <HashLink to="/#portfolio">Portfolio</HashLink>
-              </div>
-              <div className="row">
-                <HashLink to="/#contact">Contact</HashLink>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="row">
-                <HashLink to="/#education">Education</HashLink>
-              </div>
-              <div className="row">
-                <HashLink to="/#skills">Skills</HashLink>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="row">
-                <HashLink to="/#experience">Experience</HashLink>
-              </div>
-              <div className="row">
-                <a
-                  href="https://www.linkedin.com/in/kmiller29/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </div>
-            </div>
-            <div className="col-3">
-              <div className="row">
-                <a
-                  href="https://github.com/k1te-m"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  GitHub
-                </a>
-              </div>
-              <div className="row">
-                <a
-                  href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view?usp=sharing"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Resume
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </FooterStyled>
+    <FooterWrapper className="container-fluid mt-4 p-2">
+      <div className="row row-cols-3">
+        <a href="https://github.com/k1te-m" target="_blank" rel="noreferrer">
+          <i className="fab fa-github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kmiller29/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-linkedin" />
+        </a>
+        <a
+          href="https://drive.google.com/file/d/1XbOSu9JYqNGkLfgajWcSbFbJs2Bp1cXr/view"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fas fa-file" />
+        </a>
+      </div>
     </FooterWrapper>
   );
 };
