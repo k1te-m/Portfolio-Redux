7bcfa28c7841c2ea350bb878398482e65c23110d
k1te <kmiller343@gmail.com>
2021-02-17 22:34:46 -0600

    timeline data

---
+++ client/src/features/landing/timeline/timelineData.js
@@ -0,0 +1,59 @@
+export const timelineData = [
+  {
+    header: "Certificate - Full-stack Web Development",
+    text: [
+      "Areas covered: HTML5, CSS3, JavaScript, jQuery, Bootstrap, NodeJS, MySQL, MongoDB, Express, and ReactJS",
+    ],
+    image: "../images/NW-Seal.png",
+    date: "Dec 2020",
+  },
+  {
+    header: "Team Leader",
+    text: [
+      "Direct 4-person accounting team’s daily operations for Fortune 500 client ($23.9B in market capitalization)",
+      "Organize the work of the team, assign tasks, monitor all activities, and ensure timely and accurate completion",
+    ],
+    image: "../images/NT-logo.png",
+    date: "Nov 2017 - Jul 2020",
+  },
+  {
+    header: "Technical Coordinator",
+    text: [
+      "Establish that core procedures and client-specific procedures are followed on a daily and monthly basis",
+      "Serves as a key subject matter expert to clients, colleagues, and team members for clarity, understanding and information",
+    ],
+    image: "../images/NT-logo.png",
+    date: "Dec 2016 - Nov 2017",
+  },
+  {
+    header: "Senior Analyst",
+    text: [
+      "Assist partners with reconciliation of funds, resolution of exceptions, and daily processes to provide NAV reports prior to client deadlines",
+      "Lead analyst of team working on large-scale, complex and sensitive client relationship",
+    ],
+    image: "../images/NT-logo.png",
+    date: "Nov 2015 - Dec 2016",
+  },
+  {
+    header: "Analyst",
+    text: [
+      "Compute Net Asset Value (NAV) for DC/DB plans",
+      "Audit, research and resolve exceptions on assigned accounts",
+      "Analyze the impact of transactions, income positions, accruals",
+    ],
+    image: "../images/NT-logo.png",
+    date: "Dec 2014 - Nov 2015",
+  },
+  {
+    header: "Bachelor of Science in Business",
+    text: ["Economics", "Driehaus College of Business"],
+    image: "../images/DPU-Logo.png",
+    date: "Aug 2014",
+  },
+  {
+    header: "Associate of Applied Science",
+    text: ["Economics", "College of Lake County"],
+    image: "../images/clc-logo.png",
+    date: "Aug 2014",
+  },
+];
